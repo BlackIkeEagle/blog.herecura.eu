7f878a2748dfbc973237409719664b1f0070e15a
BlackEagle <ike.devolder@gmail.com>
2018-09-27 22:08:27 +0200

    change publish so it does no longer uploads everything that has changed timestamp
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@
 
 hugo
 find public -type f -exec chmod u=rw,og=r {} \;
-rsync -av --delete-after public/ herecura.eu:/var/lib/blackikeeagle-blog/data/
+rsync -rlv --delete-after public/ herecura.eu:/var/lib/blackikeeagle-blog/data/
 rm -rf public
