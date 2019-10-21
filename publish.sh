28382b7f812718732e5e16bddfa6693e40d4b155
BlackEagle <ike.devolder@gmail.com>
2019-10-21 21:32:38 +0200

    reinstalled server so now sync to web.herecura.eu
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@
 
 hugo
 find public -type f -exec chmod u=rw,og=r {} \;
-rsync -rlv --delete-after public/ herecura.eu:/var/lib/blackikeeagle-blog/data/
+rsync -rlv --delete-after public/ web.herecura.eu:/data/blackikeeagle-blog/data/
 rm -rf public
