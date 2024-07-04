fd06288e2f568a8fb3d46f8fc46a823d03763b2f
BlackEagle <ike.devolder@gmail.com>
2024-07-04 22:01:35 +0200

    add hugo-video to themes pull
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- update-theme.sh
+++ update-theme.sh
@@ -2,3 +2,4 @@
 
 git subtree pull --prefix themes/hyde https://github.com/spf13/hyde.git master --squash
 git subtree pull --prefix themes/hugo-notice https://github.com/martignoni/hugo-notice.git main --squash
+git subtree pull --prefix themes/hugo-video https://github.com/martignoni/hugo-video.git master --squash
